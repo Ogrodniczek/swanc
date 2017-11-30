48420ca76eab1a916335498484642c1bc698cf82
Tamal Saha <tamal@appscode.com>
2017-11-29 18:50:05 -0800

    Update docker build script (#14)

--- hack/docker/setup.sh
+++ hack/docker/setup.sh
@@ -4,14 +4,13 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-LIB_ROOT=$(dirname "${BASH_SOURCE}")/..
-source "$LIB_ROOT/libbuild/common/lib.sh"
-source "$LIB_ROOT/libbuild/common/pharmer_image.sh"
-
 GOPATH=$(go env GOPATH)
 SRC=$GOPATH/src
 BIN=$GOPATH/bin
 ROOT=$GOPATH
+REPO_ROOT=$GOPATH/src/github.com/pharmer/swanc
+
+source "$REPO_ROOT/hack/libbuild/common/pharmer_image.sh"
 
 APPSCODE_ENV=${APPSCODE_ENV:-dev}
 IMG=swanc
@@ -23,60 +22,58 @@ if [ -f "$DIST/.tag" ]; then
 fi
 
 clean() {
-    pushd $GOPATH/src/github.com/pharmer/swanc/hack/docker
-	rm swanc
-	popd
+    pushd $REPO_ROOT/hack/docker
+    rm -f swanc
+    popd
 }
 
 build_binary() {
-	pushd $GOPATH/src/github.com/pharmer/swanc
-	./hack/builddeps.sh
+    pushd $REPO_ROOT
+    ./hack/builddeps.sh
     ./hack/make.py build swanc
-	detect_tag $DIST/.tag
-	popd
+    detect_tag $DIST/.tag
+    popd
 }
 
 build_docker() {
-	pushd $GOPATH/src/github.com/pharmer/swanc/hack/docker
-	cp $DIST/swanc/swanc-linux-amd64 swanc
-	chmod 755 swanc
+    pushd $REPO_ROOT/hack/docker
+    cp $DIST/swanc/swanc-linux-amd64 swanc
+    chmod 755 swanc
 
-	local cmd="docker build -t appscode/$IMG:$TAG ."
-	echo $cmd; $cmd
+    local cmd="docker build -t pharmer/$IMG:$TAG ."
+    echo $cmd; $cmd
 
-	rm swanc
-	popd
+    rm swanc
+    popd
 }
 
 build() {
-	build_binary
-	build_docker
+    build_binary
+    build_docker
 }
 
 docker_push() {
-	if [ "$APPSCODE_ENV" = "prod" ]; then
-		echo "Nothing to do in prod env. Are you trying to 'release' binaries to prod?"
-		exit 0
-	fi
-
-    if [[ "$(docker images -q appscode/$IMG:$TAG 2> /dev/null)" != "" ]]; then
-        docker_up $IMG:$TAG
+    if [ "$APPSCODE_ENV" = "prod" ]; then
+        echo "Nothing to do in prod env. Are you trying to 'release' binaries to prod?"
+        exit 1
     fi
+    if [ "$TAG_STRATEGY" = "git_tag" ]; then
+        echo "Are you trying to 'release' binaries to prod?"
+        exit 1
+    fi
+    hub_canary
 }
 
 docker_release() {
-	if [ "$APPSCODE_ENV" != "prod" ]; then
-		echo "'release' only works in PROD env."
-		exit 1
-	fi
-	if [ "$TAG_STRATEGY" != "git_tag" ]; then
-		echo "'apply_tag' to release binaries and/or docker images."
-		exit 1
-	fi
-
-    if [[ "$(docker images -q appscode/$IMG:$TAG 2> /dev/null)" != "" ]]; then
-        docker push appscode/$IMG:$TAG
+    if [ "$APPSCODE_ENV" != "prod" ]; then
+        echo "'release' only works in PROD env."
+        exit 1
     fi
+    if [ "$TAG_STRATEGY" != "git_tag" ]; then
+        echo "'apply_tag' to release binaries and/or docker images."
+        exit 1
+    fi
+    hub_up
 }
 
 source_repo $@
